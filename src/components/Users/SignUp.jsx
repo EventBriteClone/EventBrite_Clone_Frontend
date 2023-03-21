80fc79f19ed682365e8db3c59a291b66ec675e88
mario-nakhla <100533874+mario-nakhla@users.noreply.github.com>
2023-03-22 01:59:15 +0200

    sign up page

--- src/components/Users/SignUp.jsx
+++ src/components/Users/SignUp.jsx
@@ -2,7 +2,7 @@ import Style from "./Style.module.css";
 import React from "react";
 
 export default function SignUP() {
-  let [email, setEmail] = React.useState("2l3w 2ho");
+  let [email, setEmail] = React.useState("");
   let [invalidEmail, setInvalid] = React.useState(false);
 
   const getInputValue = (event) => {
@@ -13,17 +13,18 @@ export default function SignUP() {
   function AddEmail() {
     const validRegex =
       /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
-    if (email.match(validRegex)) {
-      setInvalid(false);
+    if (!email) {
+      setInvalid("Field required");
+    } else if (email.match(validRegex)) {
+      setInvalid("");
     } else {
-      setInvalid(true);
+      setInvalid("Invalid email");
     }
   }
 
   return (
     <div>
-      {/* <meta charSet="UTF-8" />
+      <meta charSet="UTF-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" />
@@ -31,8 +32,8 @@ export default function SignUP() {
       <link
         href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css"
         rel="stylesheet"
-      ></link> */}
-      {/* <title>Eventbrite</title> */}
+      ></link>
+      <title>Eventbrite</title>
       <div className={Style["main"]}>
         <div className={Style["container"]}>
           <div className={Style["first-container"]}>
@@ -42,7 +43,7 @@ export default function SignUP() {
                   <a
                     href="https://www.eventbrite.com/"
                     rel="noopener noreferrer"
-                    title="Go to homepage"
+                    title="Go to/ homepage"
                   >
                     eventbrite
                   </a>
@@ -57,15 +58,15 @@ export default function SignUP() {
                     onChange={getInputValue}
                     type="email"
                     placeholder="Email address"
-                    className={Style["input"]}
+                    className={invalidEmail ? Style["error"] : Style["input"]}
                   />
                 </form>
               </div>
               <div className={Style["cont"]}>
                 <div className={Style["invalidEmail"]}>
-                  <h2>{invalidEmail ? "invalidEmail" : ""}</h2>
+                  <h2 className={Style["invalidEmail"]}>{invalidEmail}</h2>
 
-                  <button className={Style["cont"]} onClick={AddEmail}>
+                  <button className={Style["cont "]} onClick={AddEmail}>
                     Continue
                   </button>
                 </div>
@@ -75,14 +76,24 @@ export default function SignUP() {
               </div>
               <div className={Style["cont2"]}>
                 <button>
-                  <i class="ri-google-fill"></i>Sign in with Google
+                  {/* <a
+                  className="ri-google-fill"
+                  href="https://accounts.google.com/"
+                  title="Go to google account"
+                ></a> */}
+                  <i
+                    className="ri-google-fill"
+                    href="https://accounts.google.com/"
+                    title="Go to google account"
+                  ></i>
+                  Sign in with Google
                 </button>
               </div>
 
               <div className={Style["others-sign-in"]}>
                 <p>Other sign up methods</p>
                 <a
-                  class="ri-facebook-circle-fill"
+                  className="ri-facebook-circle-fill"
                   href="https://www.facebook.com/"
                   title="Go to Facebook"
                 ></a>
