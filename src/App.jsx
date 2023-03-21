c9e66e76e67e244214515d0a131fca1cc62cf279
unknown <mohammedahmed200135@gmail.com>
2023-03-21 15:14:44 +0200

    eventcreator page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,6 @@
-import Header from "./components/Layout/Header";
-import EventCard from "./components/EventCardContainer/EventCard";
-import EventCardContainer from "./components/EventCardContainer/EventCardContainer";
-import Section from "./components/Layout/Section";
-
 function App() {
-  return (
-    <>
-      <Header></Header>
-      <Section>
-        <EventCardContainer>
-          <EventCard></EventCard>
-          <EventCard></EventCard>
-          <EventCard></EventCard>
-          <EventCard></EventCard>
-          <EventCard></EventCard>
-          <EventCard></EventCard>
-        </EventCardContainer>
-      </Section>
-    </>
-  );
+  return <></>;
 }
+//=======
 
 export default App;
