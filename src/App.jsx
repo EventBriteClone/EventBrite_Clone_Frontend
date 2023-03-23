cf331a43beb32e728d1397db89363ee439e573d0
ShaimaaAhmed245 <shaimaa.Eid01@eng-st.cu.edu.eg>
2023-03-23 15:34:06 +0200

    basic info files and image folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import Header from "./components/Layout/Header";
-
+import Footer from "./components/Layout/Footer";
+import BasicInfo from "./components/Event/BasicInfo1";
+import React from "react";
 function App() {
   return (
     <>
       <Header></Header>
+      {/* <BasicInfo /> */}
+      {/* <Footer></Footer> */}
+      {/* <div className="test">
+        <label htmlFor="">
+          <span>Event Title</span>
+          <span> *</span>
+        </label>
+        <input type="text" placeholder="Enter Event Title" />
+      </div> */}
     </>
   );
 }
